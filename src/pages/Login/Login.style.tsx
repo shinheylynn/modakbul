d8c16a58c905eb791e7676e06ebed9a082d9c955
leejonghyuk <as13ljh@gmail.com>
2023-05-01 14:41:47 +0900

    "Modify : placeholder 오타수정"

--- src/pages/Login/Login.style.tsx
+++ src/pages/Login/Login.style.tsx
@@ -1,3 +1,103 @@
 import styled from 'styled-components';
 
-export const Div = styled.div``;
+export const AllWrap = styled.div`
+  padding-top: 108px;
+`;
+
+export const TitleWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const TitleImg = styled.img`
+  width: 60px;
+  height: 60px;
+  padding-right: 10px;
+`;
+
+export const Title = styled.div`
+  font-size: 60px;
+`;
+
+export const InputWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const IdInput = styled.input`
+  width: 250px;
+  height: 50px;
+  margin-top: 20px;
+  border: 1px solid #dbdbdb;
+  color : white
+  border-radius: 4px;
+  &::placeholder {
+    color:#bdbdbd;
+  }
+`;
+
+export const PwInput = styled.input`
+  width: 250px;
+  height: 50px;
+  margin-top: 20px;
+  border: 1px solid #dbdbdb;
+  border-radius: 4px;
+  &::placeholder {
+    color: #bdbdbd;
+  }
+`;
+
+export const LoginBtnWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const LoginBtn = styled.button`
+  margin-top: 40px;
+  width: 250px;
+  height: 50px;
+  border: none;
+  background-color: #89815d;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+`;
+
+export const PwSignupWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+`;
+
+export const ModifyPw = styled.div`
+  color: #a1a0a0;
+  padding-right: 35px;
+  cursor: pointer;
+`;
+
+export const Signup = styled.div`
+  color: #a1a0a0;
+  cursor: pointer;
+`;
+
+export const OrWrap = styled.div`
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Hr = styled.hr`
+  width: 115px;
+  margin-left: 4px;
+  margin-right: 4px;
+`;
+
+export const SocialWrap = styled.div`
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 14px;
+  color: #a1a0a0;
+`;
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,38 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import GoogleLoginButton from './GoogleLoginButton';
 import * as S from './Login.style';
 
 export default function Login() {
-  return <S.Div>Login</S.Div>;
+  const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
+
+  return (
+    <S.AllWrap>
+      <S.TitleWrap>
+        <S.TitleImg src="images/bonfire.png" />
+        <S.Title>모닥불</S.Title>
+      </S.TitleWrap>
+      <S.InputWrap>
+        <S.IdInput placeholder="아이디를 입력해 주세요" />
+        <S.PwInput placeholder="비밀번호를 입력해 주세요" />
+      </S.InputWrap>
+      <S.LoginBtnWrap>
+        <S.LoginBtn>로그인</S.LoginBtn>
+      </S.LoginBtnWrap>
+      <S.PwSignupWrap>
+        <S.ModifyPw>비밀번호 재설정</S.ModifyPw>
+        <S.Signup>회원가입</S.Signup>
+      </S.PwSignupWrap>
+      <S.OrWrap>
+        <S.Hr />
+        or
+        <S.Hr />
+      </S.OrWrap>
+      <S.SocialWrap>
+        <GoogleOAuthProvider clientId={clientId}>
+          <GoogleLoginButton />
+          구글 로그인
+        </GoogleOAuthProvider>
+      </S.SocialWrap>
+    </S.AllWrap>
+  );
 }
