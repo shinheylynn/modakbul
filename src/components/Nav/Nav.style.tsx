d9a6d6339a458600b9628584d915b124a7707c62
shinheylynn <heylub@gmail.com>
2023-05-05 00:43:32 +0900

    Add: Nav layout in progress

--- src/components/Nav/Nav.style.tsx
+++ src/components/Nav/Nav.style.tsx
@@ -1,3 +1,46 @@
 import styled from 'styled-components';
 
-export const Div = styled.div``;
+export const NavContainer = styled.div`
+  border: 1px black solid;
+  display: flex;
+  height: 8vh;
+`;
+
+export const LogoBox = styled.div`
+  border: 1px black solid;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 20%;
+  height: 100%;
+`;
+
+export const Logo = styled.div`
+  border: 1px blue solid;
+`;
+
+export const CategoryBox = styled.div`
+  border: 1px black solid;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 60%;
+  height: 100%;
+`;
+
+export const Categories = styled.div`
+  border: 1px red solid;
+`;
+
+export const LoginBox = styled.div`
+  border: 1px black solid;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 20%;
+  height: 100%;
+`;
+
+export const Login = styled.div`
+  border: 1px grey solid;
+`;
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,17 @@
 import * as S from './Nav.style';
 
 export default function Nav() {
-  return <S.Div>nav</S.Div>;
+  return (
+    <S.NavContainer>
+      <S.LogoBox>
+        <S.Logo>Modakbul</S.Logo>
+      </S.LogoBox>
+      <S.CategoryBox>
+        <S.Categories>Categories</S.Categories>
+      </S.CategoryBox>
+      <S.LoginBox>
+        <S.Login>Login/Logout</S.Login>
+      </S.LoginBox>
+    </S.NavContainer>
+  );
 }
