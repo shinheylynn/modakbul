b4905a0bfaf399b3bec7482cd24fb520e67fc370
shinheylynn <heylub@gmail.com>
2023-05-07 14:26:47 +0900

    Modify: main layout design 색상 변경 및 레이아웃 디자인 수정

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -5,8 +5,14 @@ export default function Main() {
   return (
     <S.MainContainer>
       <S.TitleBox>
+        <S.TitleImg src="/images/main/main_campsite.png" alt="background img" />
         <S.Title>Hello Fellow Camper !</S.Title>
         <S.SubTitle>Welcome to Modakbul</S.SubTitle>
+        <S.DescriptionBox>
+          <S.Description>
+            모닥불은 캠퍼들을 위한 캠핑장 장소 및 후기 공유 플랫폼입니다.
+          </S.Description>
+        </S.DescriptionBox>
       </S.TitleBox>
       <PostGrid />
     </S.MainContainer>
