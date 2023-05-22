39654fce4abe8c1c48711c86d4a0d4f3172eef18
shinheylynn <heylub@gmail.com>
2023-05-22 22:09:00 +0900

    Add: About 간단 레이아웃 완성 및 설명 작성 완료

--- src/pages/Main/About/About.tsx
+++ src/pages/Main/About/About.tsx
@@ -1,5 +1,32 @@
 import * as S from './About.styles';
 
 export default function About() {
-  return <div>in progress</div>;
+  return (
+    <S.AboutContainer>
+      <S.WelcomeWrapper>
+        <S.BgImgWrapper>
+          <S.BgImg src="/images/main/main_campsite.png" alt="배경이미지" />
+        </S.BgImgWrapper>
+        <S.TitleWrapper>
+          <S.AboutTitle>
+            Hello, Welcome! <br />
+            About 모닥불
+          </S.AboutTitle>
+          <S.AboutSubTitle>
+            모닥불은 2023년 5월에 team TwoPlusOne이 진행하여 배포한 첫
+            프로젝트입니다.
+            <br /> 캠퍼들의 경험을 공유할 수 있는 장을 만들기 위해 제작했습니다.
+          </S.AboutSubTitle>
+        </S.TitleWrapper>
+        <S.DownArrowBox>
+          <S.DownArrow>▼</S.DownArrow>
+          <S.DownArrow>▼</S.DownArrow>
+          <S.DownArrow>▼</S.DownArrow>
+        </S.DownArrowBox>
+      </S.WelcomeWrapper>
+      <S.AboutDescWrapper>
+        <S.AboutDesc /> 최초 배포 일자: 2023년 6월
+      </S.AboutDescWrapper>
+    </S.AboutContainer>
+  );
 }
