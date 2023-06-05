5ac9dd74a8e7123b61fc462ae532983ab728cc7c
shinheylynn <heylub@gmail.com>
2023-06-05 17:34:53 +0900

    Modify: 지도 추가 디자인 레이아웃 수정

--- src/pages/Posting/NaverMap/NaverMap.styles.tsx
+++ src/pages/Posting/NaverMap/NaverMap.styles.tsx
@@ -1,6 +1,42 @@
 import styled from 'styled-components';
 
+export const MapContainer = styled.div`
+  border: 1px solid black;
+  margin-top: 30px;
+  border: 1px solid #cccccc;
+  position: relative;
+`;
+
+export const MapLegendWrapper = styled.div`
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 50px;
+  top: -10px;
+  margin-left: 10px;
+`;
+
+export const MapLegend = styled.legend`
+  color: gray;
+  font-size: large;
+`;
+
+export const InfoWrapper = styled.div`
+  padding: 17px;
+`;
+
+export const MapInfo = styled.div`
+  margin-bottom: 7px;
+`;
+
+export const MapSubInfo = styled.div`
+  font-size: small;
+  color: gray;
+`;
+
 export const Map = styled.div`
-  width: 400px;
+  width: 80vw;
   height: 400px;
 `;
